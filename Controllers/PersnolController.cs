24b4c80c07f76561966bbb07b842f365ee79efe8
divya <divyabankar2000@gmail.com>
2023-01-20 16:48:34 +0530

    persnol controller add

---
+++ Controllers/PersnolController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GymManagement.Controllers
+{
+    public class PersnolController : Controller
+    {
+        // GET: Persnol
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
